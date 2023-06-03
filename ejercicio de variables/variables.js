461f6972d3e291b36e4c57c80b7d1d783bcb90f3
Adrian Minaya Rodriguez <78176918+AdrianMinaya@users.noreply.github.com>
2023-06-03 01:11:30 -0400

    Add files via upload

---
+++ ejercicio de variables/variables.js
@@ -0,0 +1,16 @@
+
+const fechaNac = new Date("august 11 1987")
+
+const libros = {
+    titulo: "siete habitos de la gente altamente efectiva",
+    autor: "Stephen Cobey",
+    fecha: 1998,
+    url: "ya"
+}
+
+
+const mainList = ["Adrian Minaya", 35, true, fechaNac, libros];
+
+console.log(mainList); 
+
+
